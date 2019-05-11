6e128a980489c7c170dedf80626cbfcd05e74657
CHe <che.glenn@csedge.org>
2019-05-11 10:54:29 -0400

    abogabali awoymowi

--- pokemon-starter/scripts.js
+++ pokemon-starter/scripts.js
@@ -1,3 +1,36 @@
 $(document).ready(function(){
+	$("#button1").click(getPokemon1);
+	$("#button2").click(getPokemon2);
 
-})
+	let pokemon = {
+		name:'',
+		attack:0,
+		sprites:'',
+	}
+
+	function getPokemon1(){
+		let id = Math.floor(Math.random()*800);
+		$.get(`https://pokeapi.co/api/v2/pokemon/${id}/`,function(response) {
+			console.log(response);
+			console.log(response);
+			$('#first').empty();
+			$('#first').append(`<h1>${response.name}</h1>`);
+		});
+	};
+
+	function getPokemon2(){
+		let id = Math.floor(Math.random()*800);
+		$.get(`https://pokeapi.co/api/v2/pokemon/${id}/`,function(response) {
+			console.log(response);
+			console.log(response);
+			$('#second').empty();
+			$('#second').append(`<h1>${response.name}</h1>`);
+		});
+	};
+
+	//$("battle").click(fightPokemon);
+
+	//function fightPokemon(){
+		//if 
+	//}
+});
